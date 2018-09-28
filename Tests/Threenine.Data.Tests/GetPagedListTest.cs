23079a5514758e33c5304b8997de0b9f2beefa02
Gary Woodfine <gary.woodfine@threenine.co.uk>
2018-09-28 16:09:05 +0100

    Paginate Unit test Added

--- Tests/Threenine.Data.Tests/GetPagedListTest.cs
+++ Tests/Threenine.Data.Tests/GetPagedListTest.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using TestDatabase;
 using Xunit;
 using FizzWare.NBuilder;
+using Microsoft.EntityFrameworkCore;
 
 namespace Threenine.Data.Tests
 {
@@ -19,7 +20,7 @@ namespace Threenine.Data.Tests
        
         
         [Fact]
-        public void GetProductPagedListTest()
+        public void GetProductPagedListUsingPredicateTest()
         {
             //Arrange 
             var uow = new UnitOfWork<TestDbContext>(_testFixture.Context);
@@ -28,6 +29,22 @@ namespace Threenine.Data.Tests
             var productList = repo.GetList(predicate: x => x.CategoryId == 1).Items;
             //Assert
             Assert.Equal(5, productList.Count);
+        }
+
+        [Fact]
+        public void GetPagedListIncludesTest()
+        {
+            var uow = new UnitOfWork<TestDbContext>(_testFixture.Context);
+
+            var cats = uow.GetRepository<TestCategory>().GetList(include: source =>
+                source.Include(x => x.Products).ThenInclude(prod => prod.Category), size:5);
+            
+            Assert.IsAssignableFrom<Paging.Paginate<TestCategory>>(cats);
+            
+            Assert.Equal(cats.Count, 20);
+            Assert.Equal(cats.Pages, 4);
+            Assert.Equal(cats.Items.Count, 5);
+            
         }
 
        
