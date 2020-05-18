9c1583753bffb62023603e41b3813bce3d223283
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-05-18 20:12:11 +0100

    WIP - Added invalid operation test

--- tests/Threenine.Data.Tests/UpdateTests.cs
+++ tests/Threenine.Data.Tests/UpdateTests.cs
@@ -3,11 +3,12 @@ using FizzWare.NBuilder;
 using TestDatabase;
 using Threenine.Data.Tests.TestFixtures;
 using Xunit;
+using Xunit.Sdk;
 
 namespace Threenine.Data.Tests
 {
     [Collection(GlobalTestStrings.ProductCollectionName)]
-    public class UpdateTests: IDisposable
+    public class UpdateTests : IDisposable
     {
         private readonly SqlLiteWith20ProductsTestFixture _fixture;
 
@@ -23,26 +24,66 @@ namespace Threenine.Data.Tests
             using var uow = new UnitOfWork<TestDbContext>(_fixture.Context);
             var repo = uow.GetRepository<TestProduct>();
 
-            var product = repo.GetSingleOrDefault(x => x.Id == 1 );
+            var product = repo.GetSingleOrDefault(x => x.Id == 1);
 
             Assert.IsAssignableFrom<TestProduct>(product);
-            
+
             product.Name = newProductName;
-            
+
             repo.Update(product);
-           
+
             uow.Commit();
 
-           var updatedProduct = repo.GetSingleOrDefault(x => x.Id == 1);
-           
-           Assert.Equal(updatedProduct.Name, newProductName);
+            var updatedProduct = repo.GetSingleOrDefault(x => x.Id == 1);
+
+            Assert.Equal(updatedProduct.Name, newProductName);
+        }
+
+        [Fact]
+        public void ShouldUpdateMultipleProductsByParams()
+        {
+            const string newProduct1Name = "Foo Bar";
+            const string newProduct2Name = "Bar Foo";
+
+            using var uow = new UnitOfWork<TestDbContext>(_fixture.Context);
+            var repo = uow.GetRepository<TestProduct>();
+
+            var product1 = repo.GetSingleOrDefault(x => x.Id == 1);
+            var product2 = repo.GetSingleOrDefault(x => x.Id == 2);
+
+
+            product1.Name = newProduct1Name;
+            product2.Name = newProduct2Name;
+
+            repo.Update(product1, product2);
+
+            uow.Commit();
+
+            var updatedProduct1 = repo.GetSingleOrDefault(x => x.Id == 1);
+            var updatedProduct2 = repo.GetSingleOrDefault(x => x.Id == 2);
+            Assert.Equal(updatedProduct1.Name, newProduct1Name);
+            Assert.Equal(updatedProduct2.Name, newProduct2Name);
+        }
+
+        [Fact]
+        public void ShouldThrowInvalidOperationException()
+        {
+            const string newProductName = "Foo Bar";
+            using var uow = new UnitOfWork<TestDbContext>(_fixture.Context);
+            var repo = uow.GetRepository<TestProduct>();
+
+            var product = repo.GetSingleOrDefault(x => x.Id == 1, enableTracking: false);
+
+            Assert.IsAssignableFrom<TestProduct>(product);
+
+            product.Name = newProductName;
+
+            Assert.Throws<InvalidOperationException>(() => repo.Update(product));
         }
 
         public void Dispose()
         {
             _fixture?.Dispose();
         }
-        
-        
     }
 }
