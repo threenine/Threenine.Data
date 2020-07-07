aad459b73c05cea8b70e47a074f76df5d42d6671
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-07-07 14:00:26 +0100

    WIP - Added some additional unit tests

--- src/UnitOfWork.cs
+++ src/UnitOfWork.cs
@@ -17,9 +17,11 @@
 
 using System;
 using System.Collections.Generic;
-using System.Runtime.Serialization;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+
+[assembly: InternalsVisibleTo("Threenine.Data.Tests")]
 
 namespace Threenine.Data
 {
@@ -35,41 +37,28 @@ namespace Threenine.Data
 
         public IRepository<TEntity> GetRepository<TEntity>() where TEntity : class
         {
-
             return (IRepository<TEntity>) GetOrAddRepository(typeof(TEntity), new Repository<TEntity>(Context));
         }
 
 
         public IRepositoryAsync<TEntity> GetRepositoryAsync<TEntity>() where TEntity : class
         {
-           return (IRepositoryAsync<TEntity>) GetOrAddRepository( typeof(TEntity),  new RepositoryAsync<TEntity>(Context));
+            return (IRepositoryAsync<TEntity>) GetOrAddRepository(typeof(TEntity),
+                new RepositoryAsync<TEntity>(Context));
         }
 
         public IRepositoryReadOnly<TEntity> GetReadOnlyRepository<TEntity>() where TEntity : class
         {
-           return (IRepositoryReadOnly<TEntity>)GetOrAddRepository( typeof(TEntity),  new RepositoryReadOnly<TEntity>(Context));
+            return (IRepositoryReadOnly<TEntity>) GetOrAddRepository(typeof(TEntity),
+                new RepositoryReadOnly<TEntity>(Context));
         }
 
         public IRepositoryReadOnlyAsync<TEntity> GetReadOnlyRepositoryAsync<TEntity>() where TEntity : class
         {
-            return (IRepositoryReadOnlyAsync<TEntity>) GetOrAddRepository( typeof(TEntity), new RepositoryReadOnlyAsync<TEntity>(Context));
+            return (IRepositoryReadOnlyAsync<TEntity>) GetOrAddRepository(typeof(TEntity),
+                new RepositoryReadOnlyAsync<TEntity>(Context));
         }
 
-        private object GetOrAddRepository(Type type, object repo)
-        {
-            if (_repositories == null)
-            {
-               _repositories = new Dictionary<(Type type, string Name), object>();
-            }
-            
-            if (!_repositories.TryGetValue((type, repo.GetType().FullName), out var repository))
-            {
-                _repositories.Add((type, repo.GetType().FullName), repo);
-               
-            }
-
-            return repo;
-        }
         public TContext Context { get; }
 
         public int Commit(bool autoHistory = false)
@@ -88,6 +77,15 @@ namespace Threenine.Data
         public void Dispose()
         {
             Context?.Dispose();
+        }
+
+        internal object GetOrAddRepository(Type type, object repo)
+        {
+            _repositories ??= new Dictionary<(Type type, string Name), object>();
+
+            if (_repositories.TryGetValue((type, repo.GetType().FullName), out var repository)) return repository;
+            _repositories.Add((type, repo.GetType().FullName), repo);
+            return repo;
         }
     }
 }
