3b16eed6f58c69ca8b5126e271845d49de264969
Gary Woodfine <gary.woodfine@threenine.co.uk>
2022-05-06 17:19:04 +0100

    change to paginations

--- src/Paging/IPaginateExtension.cs
+++ src/Paging/IPaginateExtension.cs
@@ -22,13 +22,13 @@ namespace Threenine.Data.Paging
 {
     public static class PaginateExtensions
     {
-        public static IPaginate<T> ToPaginate<T>(this IEnumerable<T> source, int index, int size, int from = 1)
+        public static IPaginate<T> ToPaginate<T>(this IEnumerable<T> source, int index, int size, int from = 0)
         {
             return new Paginate<T>(source, index, size, from);
         }
 
         public static IPaginate<TResult> ToPaginate<TSource, TResult>(this IEnumerable<TSource> source,
-            Func<IEnumerable<TSource>, IEnumerable<TResult>> converter, int index, int size, int from = 1)
+            Func<IEnumerable<TSource>, IEnumerable<TResult>> converter, int index, int size, int from = 0)
         {
             return new Paginate<TSource, TResult>(source, converter, index, size, from);
         }
